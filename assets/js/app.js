5d81ba48fe4819441c9d402b4824cf92cccc1503
connor scoma <connorscoma@connors-MacBook-Pro.local>
2019-05-14 12:43:51 -0500

    adding html and firebase

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,14 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBHCoWWp3x4nmH2Th5YeS6VTsOfTL99Ar0",
+    authDomain: "rps-online-fbad4.firebaseapp.com",
+    databaseURL: "https://rps-online-fbad4.firebaseio.com",
+    projectId: "rps-online-fbad4",
+    storageBucket: "rps-online-fbad4.appspot.com",
+    messagingSenderId: "669051267276",
+    appId: "1:669051267276:web:ca154a50e21c6085"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
 console.log('hello World')
